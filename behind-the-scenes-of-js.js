d5f0cd0ec7b53b83096238875c1d1b5654487dc0
the-ajay-panigrahi <ajaybpanigrahi@gmail.com>
2025-04-24 23:25:11 +0530

    bts of js

---
+++ behind-the-scenes-of-js.js
@@ -0,0 +1,94 @@
+// console.log(`You are learning on the platform called ${platformName}`);
+
+// var platformName = "Chai Code"
+
+// console.log(`You are learning on the platform called ${platformName}`);
+
+// instructor = "Ajay"
+
+// console.log(`Current instructor is ${instructor}`);
+
+// var instructor = "Saloni"
+
+// console.log(`Current instructor is ${instructor}`);
+
+
+// console.log("JavaScript Execution Starts");
+
+// var greeting = "Hello from the global scope!";
+
+// function sayHello() {
+//     console.log("This message is from the global function.");
+// }
+
+// console.log(greeting);
+
+// sayHello();
+
+// console.log("JavaScript Execution Ends");
+
+
+
+// console.log("JavaScript Execution Starts");
+
+// var greeting = "Hello from the global scope!";
+
+// console.log(greeting);
+
+// sayHello();
+
+// console.log("JavaScript Execution Ends");
+
+// function sayHello() {
+//     console.log("This message is from the global function.");
+// }
+
+
+// console.log("JavaScript Execution Starts");
+
+// var greeting = "Hello from the global scope!";
+
+// console.log(greeting);
+
+// var sayHello = () => {
+//     console.log("This message is from the global function.");
+// }
+
+// sayHello();
+
+// console.log("JavaScript Execution Ends");
+
+
+// console.log("JavaScript Execution Starts");
+
+// var greeting = "Hello from the global scope!";
+
+// console.log(greeting);
+
+// sayHello();
+
+// console.log("JavaScript Execution Ends");
+
+// ()=>{
+//     console.log("This message is from the global function.");
+// }
+
+
+// console.log(`Current instructor is ${instructor}`);
+
+// let instructor = "Saloni"
+
+// console.log(`Current instructor is ${instructor}`);
+
+
+console.log("Grind is ON!!");
+
+setTimeout(() => {
+    console.log("Chai Code (setTimeOut)");
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log("Coding Hero (Promise)");
+})
+
+console.log("Grind is OFF!!");
